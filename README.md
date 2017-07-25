51af0bbe7340008186b8f377a82b534308bbe1c9
hayato4331 <oohasi3939@gmail.com>
2017-07-25 12:17:34 +0900

    test

---
+++ README.md
@@ -0,0 +1,8 @@
+# PrimeNumber
+This program is a program to input prime numbers.
+Enter prime numbers until you make a mistake.
+Press the Enter key when you are done.
+Then I enter the next prime number.
+
+## Behavior
+In this program
---
+++ main.py
@@ -0,0 +1,25 @@
+#!/bin/python
+import random
+
+def is_prime(q,k=50):
+    q = abs(q)
+    if q == 2 : return True
+    if q < 2 or q&1 == 0: return False
+
+    d = (q-1) >> 1
+    while d&1 ==0:
+        d >>= 1
+
+    for i in range(k):
+        a = random.randint(1,q-1)
+        t = d
+        y =pow(a,t,q)
+
+    while t != q-1 and y != 1 and y != q-1 :
+        y = pow(y,2,q)
+        t <<= 1
+
+    if y != q-1 and t&1 == 0:
+        return False
+
+    return True
