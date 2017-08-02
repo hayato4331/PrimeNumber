1984d945afaa9d1435788b54adcfbc942f535d68
hayato4331 <oohasi3939@gmail.com>
2017-08-02 17:43:52 +0900

    Adopt Fermat's little theorem

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 #!/bin/python
+#-*- coding:utf-8 -*-
 
 # Python's studu now...
 
-def TF(X):
-    if X >=10:
-        return "true"
-    else:
-        return "false"
+#import
 
+#function
+def fermat(p,n):
+    if (p**(n-1))%n == 1 :
+        ans ="true"
 
+    elif p**(n-1)%p == 0:
+        ans = "false"
+
+    else :
+        ans = "error"
+
+    return ans
+
+#main
+FastPrime = 2
+prime = FastPrime
+
+print("Number?")
 num=int(input())
-print(TF(num))
+print(fermat(prime,num))
