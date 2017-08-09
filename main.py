628f6f8327c992eedc2dcaccacee322d3f483b8f
hayato4331 <oohasi3939@gmail.com>
2017-08-09 10:46:41 +0900

    no change...

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 #!/bin/python
 #-*- coding:utf-8 -*-
 
-# Python's studu now...
-
-#import
-
-#function
-
 #main
 FastPrime = 2
 prime = FastPrime
@@ -28,24 +22,3 @@ while num < count :
 #        else :
 #            print("error")
 #            break
-
-#BeforePlogram
-## def fermat(p,n):
-##     if (p**(n-1))%n == 1 :
-##         ans ="true"
-##
-##     elif p**(n-1)%p == 0:
-##         ans = "false"
-##
-##     else :
-##         ans = "error"
-##
-##     return ans
-##
-## #main
-## FastPrime = 2
-## prime = FastPrime
-##
-## print("Number?")
-## num=int(input())
-## print(fermat(prime,num))
